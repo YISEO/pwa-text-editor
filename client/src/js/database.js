124fa626a2c25419fc9068fe30e03261c62269a3
Yiseo Kwon <yiseo1122@gmail.com>
2023-10-26 23:10:24 -0500

    feat: Added logic for put and get database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,31 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // Create a connection to the database and version we want to use
+  const jateDB = await openDB('jate', 1);
+  // Create a new transaction and specify the database
+  const tx = jateDB.transaction('jate', 'readwrite');
+  // Open up the desired object store
+  const store = tx.objectStore('jate');
+  // Use the .put() method to modify the data
+  const request = store.put({ id: 1, value: content });
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
+
+
+export const getDb = async () => {
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  // Get all request
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('Data read from database', result);
+  return result.value;
+};
 
 initdb();
